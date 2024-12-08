64dd657d67a17a1cb6409d6d53816812cdc801db
reigigax <134461486+reigigax@users.noreply.github.com>
2024-12-08 03:03:14 -0300

    Base de Datos reconfigurada

--- solicitudes/forms.py
+++ solicitudes/forms.py
@@ -1,26 +1,46 @@
 from django import forms
 from django.utils.timezone import now, localtime
-from .models import Solicitud, Solicitante, Detalle_Direccion, Asistente_Social, Direccion, Region, Comuna, Calle
+from .models import Solicitud, Solicitante, Asistente_Social
 
 
 class SolicitudForm(forms.ModelForm): 
     class Meta:
         model = Solicitud
-        fields = ['nombre', 'beneficio', 'cantidadBeneficios', 'hora', 'fecha', 'fkRutAsistenteSocial', 'fkRutSolicitante']
+        fields = ['nombreCasoSolicitud', 'beneficio', 'cantidadBeneficios', 'hora', 'fecha', 'fkRutAsistenteSocial', 'fkRutSolicitante', 'descripcionBeneficio','nombreCalle','numeroCalle', 'nombreAvenida','nombreComuna','nombreRegion','casa', 'poblacion_villa_condominio', 'unidadVecinal', 'depto', 'block_edificio']
         labels = {
-            'nombre':'Nombre del Caso',
+            'nombreCasoSolicitud':'Nombre del Caso',
             'beneficio':'Tipo de Beneficio',
             'cantidadBeneficios':'N° Beneficios',
             'fkRutAsistenteSocial':'Asistente Social',
-            'fkRutSolicitante':'Solicitante'
+            'fkRutSolicitante':'Solicitante',
+            'descripcionBeneficio':'Descripción del Beneficio',
+            'casa': 'Casa',
+            'poblacion_villa_condominio': 'Población/Villa/Condominio',
+            'unidadVecinal': 'Unidad Vecinal',
+            'depto': 'Departamento',
+            'block_edificio': 'Block/Edificio'
         }
         widgets = {
-            'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa el Nombre del Caso'}),
+            'nombreCasoSolicitud': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa el Nombre del Caso'}),
             'beneficio': forms.TextInput(attrs={'class': 'form-control','placeholder':'Tipo/s de Beneficio/s'}),
             'cantidadBeneficios': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'N° Beneficios'}),
+            'descripcionBeneficio': forms.Textarea(attrs={'rows': 4 ,'class': 'form-control', 'placeholder': 'Descripción del Beneficio y Detalles del Solicitante'}),
 
             'fecha': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
             'hora': forms.TimeInput(format='%H:%M', attrs={'type': 'time','class': 'form-control'}),
+            
+            'nombreCalle': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Nombre de la Calle'}),
+            'numeroCalle': forms.NumberInput(attrs={'class':'form-control', 'placeholder':'N° de la Calle'}),
+            'nombreAvenida': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Nombre de la Avenida'}),
+
+            'nombreComuna': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Nombre de la Comuna'}),
+            'nombreRegion': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Nombre de la Region'}),
+
+            'casa': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Ingresar Casa'}),
+            'poblacion_villa_condominio': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Población/Villa/Condominio'}),
+            'unidadVecinal': forms.NumberInput(attrs={'class': 'form-control', 'placeholder':'N° Unidad Vecinal'}),
+            'depto': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Ingresar Departamento'}),
+            'block_edificio': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Block/Edificio'}),
 
             'fkRutAsistenteSocial': forms.Select(attrs={'class': 'form-select'}),
             'fkRutSolicitante': forms.Select(attrs={'class': 'form-select'}),
@@ -45,37 +65,13 @@ class SolicitudForm(forms.ModelForm):
         return cleaned_data
 
 
-class CalleForm(forms.ModelForm):
-    class Meta:
-        model = Calle
-        fields = ['nombre', 'numero', 'avenida']
-
-class ComunaForm(forms.ModelForm):
-    class Meta:
-        model = Comuna
-        fields = ['nombre']
-
-class RegionForm(forms.ModelForm):
-    class Meta:
-        model = Region
-        fields = ['nombre']
-
-class DireccionForm(forms.ModelForm):
-    class Meta:
-        model = Direccion
-        fields = ['fkRegion', 'fkComuna', 'fkCalle']
-
 class AsistenteSocialForm(forms.ModelForm):
     class Meta:
         model = Asistente_Social
-        fields = ['rutAsistenteSocial', 'nombres', 'apellidos', 'telefono', 'edad', 'correo', 'fkDireccion']
+        fields = ['rutAsistenteSocial', 'nombres', 'apellidos', 'telefono', 'edad', 'correo']
 
-class DetalleDireccionForm(forms.ModelForm):
-    class Meta:
-        model = Detalle_Direccion
-        fields = ['casa', 'poblacion_villa_condominio', 'unidadVecinal', 'depto', 'block_edificio']
 
 class SolicitanteForm(forms.ModelForm):
     class Meta:
         model = Solicitante
-        fields = ['rutSolicitante', 'primerNombre', 'segundoNombre', 'primerApellido', 'segundoApellido', 'telefono', 'edad', 'correo', 'grupoFamiliar', 'fkDireccion', 'fkDetalleDireccion']
+        fields = ['rutSolicitante', 'primerNombre', 'segundoNombre', 'primerApellido', 'segundoApellido', 'telefono', 'edad', 'correo', 'grupoFamiliar']
--- solicitudes/models.py
+++ solicitudes/models.py
@@ -2,43 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Calle(models.Model):
-    idCalle = models.AutoField(db_column='id_calle', primary_key=True)
-    nombre = models.CharField(max_length=90, blank=False, null=False)
-    numero = models.IntegerField(blank=False, null=False)
-    avenida = models.CharField(max_length=90, blank=False, null=False)
-
-    def __str__(self):
-        formato_numero = str(self.numero)
-        return str(self.nombre +" "+ formato_numero +", "+ self.avenida)
-
-
-class Comuna(models.Model):
-    idComuna = models.AutoField(db_column='id_comuna', primary_key=True)
-    nombre = models.CharField(max_length=90, blank=False, null=False)
-
-    def __str__(self):
-        return str(self.nombre)
-
-
-class Region(models.Model):
-    idRegion = models.AutoField(db_column='id_region', primary_key=True)
-    nombre = models.CharField(max_length=90, blank=False, null=False)
-
-    def __str__(self):
-        return str(self.nombre)
-
-
-class Direccion(models.Model):
-    idDireccion = models.AutoField(db_column='id_direccion', primary_key=True)
-    fkComuna = models.ForeignKey('Comuna', on_delete=models.CASCADE, db_column='id_comuna')
-    fkRegion = models.ForeignKey('Region', on_delete=models.CASCADE, db_column='id_region')
-    fkCalle = models.ForeignKey('Calle', on_delete=models.CASCADE, db_column='id_calle')
-
-    def __str__(self):
-        return f"{self.fkCalle}, {self.fkComuna} ({self.fkRegion})"
-
-
 class Asistente_Social(models.Model):
     rutAsistenteSocial = models.CharField(max_length=13, primary_key=True)
     nombres = models.CharField(max_length=150, blank=False, null=False)
@@ -46,22 +9,9 @@ class Asistente_Social(models.Model):
     telefono = models.IntegerField(blank=False, null=False)
     edad = models.IntegerField(blank=False, null=False)
     correo = models.CharField(max_length=150, blank=False, null=False)
-    fkDireccion = models.ForeignKey('Direccion', on_delete=models.CASCADE, db_column='id_direccion')
 
     def __str__(self):
         return str(self.nombres +" "+ self.apellidos)
-
-
-class Detalle_Direccion(models.Model):
-    idDetalleDireccion = models.AutoField(db_column='id_detalle_direccion', primary_key=True)
-    casa = models.CharField(max_length=90, blank=False, null=False)
-    poblacion_villa_condominio = models.CharField(max_length=90, blank=False, null=False)
-    unidadVecinal = models.CharField(max_length=90, blank=False, null=False)
-    depto = models.CharField(max_length=90, blank=False)
-    block_edificio = models.CharField(max_length=90, blank=False)
-
-    def __str__(self):
-        return str(self.casa +" "+ self.poblacion_villa_condominio +" "+ self.unidadVecinal +" "+ self.depto +" "+ self.block_edificio)
 
 
 class Solicitante(models.Model):
@@ -74,8 +24,6 @@ class Solicitante(models.Model):
     edad = models.IntegerField(blank=False, null=False)
     correo = models.CharField(max_length=150, blank=False, null=False)
     grupoFamiliar = models.CharField(max_length=150, blank=False, null=False)
-    fkDireccion = models.ForeignKey('Direccion', on_delete=models.CASCADE, db_column='id_direccion')
-    fkDetalleDireccion = models.ForeignKey('Detalle_Direccion', on_delete=models.CASCADE, db_column='id_detalle_direccion')
 
     def __str__(self):
         return str(self.rutSolicitante)
@@ -83,12 +31,25 @@ class Solicitante(models.Model):
 
 class Solicitud(models.Model):
     numeroSolicitud = models.AutoField(db_column='n_solicitud', primary_key=True)
-    nombre = models.CharField(max_length=150, blank=False, null=False)
+    nombreCasoSolicitud = models.CharField(max_length=150, blank=False, null=False)
     beneficio = models.CharField(max_length=250, blank=False, null=False)
-    descripcionBeneficio = models.CharField(max_length=500, blank=True, null=True)
+    descripcionBeneficio = models.CharField(max_length=2500, blank=True, null=True)
     cantidadBeneficios = models.IntegerField(blank=False, null=False)
     hora = models.TimeField(blank=False, null=False)
     fecha = models.DateField(blank=False, null=False)
+
+    nombreCalle = models.CharField(max_length=90, blank=False, null=False)
+    numeroCalle = models.IntegerField(blank=False, null=False)
+    nombreAvenida = models.CharField(max_length=90, blank=False, null=False)
+
+    nombreComuna = models.CharField(max_length=90, blank=False, null=False)
+    nombreRegion = models.CharField(max_length=90, blank=False, null=False)
+
+    casa = models.CharField(max_length=90, blank=False, null=False)
+    poblacion_villa_condominio = models.CharField(max_length=90, blank=False, null=False)
+    unidadVecinal = models.CharField(max_length=90, blank=False, null=False)
+    depto = models.CharField(max_length=90, blank=False, default='Sin Especificar')
+    block_edificio = models.CharField(max_length=90, blank=False, default='Sin Especificar')
 
     fkRutAsistenteSocial = models.ForeignKey('Asistente_Social', on_delete=models.CASCADE, db_column='rutAsistenteSocial')
     fkRutSolicitante = models.ForeignKey('Solicitante', on_delete=models.CASCADE, db_column='rutSolicitante')
